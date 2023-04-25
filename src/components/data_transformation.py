0072b9ad068fbd290befcfe9b13f40eace91f651
SaiKumar1605 <ramagiri.saikumar09@gmail.com>
2023-04-25 22:14:35 +0530

    tranfrom_changes

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -26,15 +26,14 @@ class DataTransformation:
         try:
             logging.info('Data Transformation initiated')
             # Define which columns should be ordinal-encoded and which should be scaled
-            categorical_cols = ['Road_traffic_density', 'multiple_deliveries']
-            numerical_cols = ['Delivery_person_Age','Delivery_person_Ratings','Prep_time','Distance','Vehicle_condition_categorie']
-            cat_nom_cols = ['Weather_conditions','Type_of_order','Type_of_vehicle','Festival','City']
+            ordinal_cols = ['Road_traffic_density']
+            numerical_cols = ['Delivery_person_Age','Delivery_person_Ratings','Prep_time','Vehicle_condition','multiple_deliveries','Distance']
+            nominal_cols = ['Weather_conditions','Type_of_order','Type_of_vehicle','Festival','City']
             
             # Define the custom ranking for each ordinal variable
-            ['Jam', 'High', 'Medium', 'Low']
             Road_traffic_density_categories = ['Low','Medium','High','Jam']
             #Vehicle_condition_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
-            multiple_deliveries_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+            #multiple_deliveries_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
             
             logging.info('Pipeline Initiated')
 
@@ -49,10 +48,20 @@ class DataTransformation:
             )
 
             # Categorigal Pipeline
-            cat_pipeline=Pipeline(
+            ord_pipeline=Pipeline(
                 steps=[
                 ('imputer',SimpleImputer(strategy='most_frequent')),
-                ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
+                ('ordinalencoder',OrdinalEncoder(categories=[Road_traffic_density_categories])),
+                ('scaler',StandardScaler())
+                ]
+
+            )
+
+            # Categorigal Pipeline
+            nom_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('onehotencoder',OneHotEncoder(sparse=False)),
                 ('scaler',StandardScaler())
                 ]
 
@@ -60,7 +69,8 @@ class DataTransformation:
 
             preprocessor=ColumnTransformer([
             ('num_pipeline',num_pipeline,numerical_cols),
-            ('cat_pipeline',cat_pipeline,categorical_cols)
+            ('ord_pipeline',cat_pipeline,ordinal_cols),
+            ('nom_pipeline',nom_pipeline,nominal_cols)
             ])
             
             return preprocessor
@@ -86,7 +96,8 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformation_object()
 
             target_column_name = 'Time_taken_min'
-            drop_columns = [target_column_name,'ID','Delivery_person_ID','Restaurant_latitude','Restaurant_longitude','Delivery_location_latitude','Delivery_location_longitude','Order_Date','Time_Orderd','Time_Order_picked']
+            #drop_columns = [target_column_name,'ID','Delivery_person_ID','Restaurant_latitude','Restaurant_longitude','Delivery_location_latitude','Delivery_location_longitude','Order_Date','Time_Orderd','Time_Order_picked']
+            drop_columns = [target_column_name]
 
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df=train_df[target_column_name]
